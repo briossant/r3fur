56b28f00f1816ac9c7181845f0b7d7ca9bf9d667
Crosson Brieuc <crosson.brieuc@gmail.com>
2023-11-02 15:33:34 +0100

    setup shader mat

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,20 @@
 import {OrbitControls, Box} from '@react-three/drei'
+import * as THREE from 'three';
+
 
 export default function Experience() {
+
+    const material = new THREE.ShaderMaterial({
+
+        uniforms: {
+            time: {value: 1.0},
+            resolution: {value: new THREE.Vector2()}
+        },
+
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
+
+    });
 
     return <>
 
@@ -8,13 +22,11 @@ export default function Experience() {
 
 
         {/*<axesHelper args={[20, 20, 20]}/>*/}
-
+        <OrbitControls />
         <directionalLight color={"#ffffff"} position={[-1, -2, -3]} intensity={0.3} shadow-normalBias={0.04} />
         <directionalLight color={"#ffffff"} castShadow position={[1, 2, 3]} intensity={1.5} shadow-normalBias={0.04} />
         <ambientLight intensity={0.5} />
 
-        <Box >
-            <meshStandardMaterial />
-        </Box>
+        <Box material={material} />
     </>;
 }
